84b7cb6c9b5217392a50c3d1ec0f4039701760f2
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-13 02:16:35 +0600

    get user api

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { test, updateUser, deleteUser, getUserListings } = require('../controllers/userController');
+const { test, updateUser, deleteUser, getUserListings, getUser } = require('../controllers/userController');
 
 const verifyUser = require('../utils/verifyUser');
 const router = express.Router();
@@ -9,5 +9,6 @@ router.get('/test', test);
 router.post('/update/:id', verifyUser, updateUser);
 router.delete('/delete/:id', verifyUser, deleteUser);
 router.get('/listings/:id', verifyUser, getUserListings);
+router.get('/:id', verifyUser, getUser);
 
 module.exports = router;
