382da294fbc4644305331064b21979ceebd648ca
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-12 23:41:53 +0600

    update server all listing updated

--- src/routers/listingRoutes.js
+++ src/routers/listingRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createListing, deleteListing } = require("../controllers/listingController");
+const { createListing, deleteListing, updateListing, getListing } = require("../controllers/listingController");
 const verifyUser = require("../utils/verifyUser");
 
 const router = express.Router();
@@ -7,7 +7,8 @@ const router = express.Router();
 router.post('/create', verifyUser, createListing);
 router.delete('/delete/:id', verifyUser, deleteListing);
 router.post('/update/:id', verifyUser, updateListing);
-// router.get('/get/:id', getListing);
+router.get('/get/:id', getListing);
 // router.get('/get', getListings);
+
 
 module.exports = router;
