f1630e94a924fa19eaa62fe77067a677206626d8
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2023-12-16 19:34:07 +0600

    added

--- api/config/db.js
+++ api/config/db.js
@@ -1,15 +1,15 @@
-// const mongoose = require("mongoose");
-// const { mongodbURL } = require("../secret");
-// const connectDatabase = async () => {
-//     try {
-//         await mongoose.connect(mongodbURL)
-//         console.log("connection to db is successfully established");
+const mongoose = require("mongoose");
+const { mongodbURL } = require("../secret");
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(mongodbURL)
+        console.log("connection to db is successfully established");
 
-//         mongoose.connection.on('error', (error) => {
-//             console.error('db connection error:', error);
-//         })
-//     } catch (error) {
-//         console.error('could not connect in db:', error.toString());
-//     }
-// }
-// module.exports=connectDatabase;
+        mongoose.connection.on('error', (error) => {
+            console.error('db connection error:', error);
+        })
+    } catch (error) {
+        console.error('could not connect in db:', error.toString());
+    }
+}
+module.exports=connectDatabase;
--- api/index.js
+++ api/index.js
@@ -1,52 +1,10 @@
-const mongoose = require("mongoose");
+
 const port = process.env.PORT || 5000;
-const express = require('express');
-const app = express();
+const app = require('./app');
+const connectDatabase = require('./config/db');
 
-const cors = require('cors');
-const router = require('./routers/userRouter');
-const authRouter = require('./routers/authRoutes');
-const listingRouter = require('./routers/listingRoutes');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
-
-mongoose
-    .connect(process.env.MONGODB_ATLAS_URL)
-    .then(() => {
-        console.log('Connected to MongoDB! database');
-    })
-    .catch((err) => {
-        console.log(err);
-    });
-
-
-//middleware
-app.use(cors());
-app.use(express.json());
-app.use(cookieParser());
-
-
-// Serve static files from the 'Client/dist' directory
-
-
-app.use('/api/user', router)
-app.use('/api/auth', authRouter)
-app.use('/api/listing', listingRouter);
-
-app.get('/', (req, res) => {
-    res.send('Server is running for the real state project')
-})
-
-app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || "internal server error";
-    return res.status(statusCode).json({
-        success: false,
-        statusCode: statusCode,
-        message,
-    })
-})
 
 app.listen(port, async () => {
     console.log(`real-state server running port: ${port}`);
+    await connectDatabase();
 })
